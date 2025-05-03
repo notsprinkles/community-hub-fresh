86e344308a50b7037e29b4bae93007b9e6fc32f4
notsprinkles <j.laraia@icloud.com>
2025-05-03 08:39:38 -0400

    Update API base URL to Render

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = "https://community-hub-api.onrender.com/api";
 
 function App() {
   const [formType, setFormType] = useState("login");
